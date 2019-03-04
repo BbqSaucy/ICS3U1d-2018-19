90b36ee4e68869ad1c66d3231bcac4a367b17b38
robuntu <robuntu@stro.ycdsb.ca>
2019-03-04 11:08:15 -0500

    commit

---
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -0,0 +1,36 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# define your draw functions
+def draw_cloud():
+    arcade.draw_circle_filled(200, 500, 50, arcade.color.ANTIQUE_WHITE)
+    arcade.draw_circle_filled(250, 550, 50, arcade.color.ANTIQUE_WHITE)
+    arcade.draw_circle_filled(300, 500, 50, arcade.color.ANTIQUE_WHITE)
+
+def draw_rollinghills():
+    arcade.draw_circle_filled(50, -50, 300, arcade.color.GREEN)
+    arcade.draw_circle_filled(750, -50, 400, arcade.color.GREEN)
+    arcade.draw_circle_filled(400, 100, 150, arcade.color.GREEN)
+
+def draw_tree():
+    arcade.draw_rectangle_filled(400, 250, 40, 80, arcade.color.BROWN)
+    arcade.draw_circle_filled(400, 330, 30, arcade.color.APPLE_GREEN)
+
+def main():
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
+    arcade.set_background_color(arcade.color.DARK_BLUE)
+    arcade.start_render()
+
+   # call your draw functions
+    draw_cloud()
+    draw_rollinghills()
+    draw_tree()
+    # Finish and run
+    arcade.finish_render()
+    arcade.run()
+
+
+# Call the main function to get the program started.
+main()
